20021e36afdefe9a172d4cedafcd8c019deb3638
matharotheelf <mrthomasjacoblawrence@hotmail.co.uk>
2019-04-12 18:02:34 +0100

    Can delete items from basket

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :shop_items
   resources :basket_items
+  post 'basket_items/delete', to: 'basket_items#delete'
   root 'shop_items#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- spec/features/basket_items_spec.rb
+++ spec/features/basket_items_spec.rb
@@ -7,5 +7,13 @@ RSpec.feature 'Basket', type: :feature do
         'Almond Toe Court Shoes, Patent Black', count: 2
       )
     end
+    scenario 'User can delete items from basket' do
+      visit '/'
+      click_button 'Almond Toe Court Shoes, Patent Black'
+      click_button 'Delete 1'
+      expect(page).to have_content(
+        'Almond Toe Court Shoes, Patent Black', count: 1
+      )
+    end
   end
 end
