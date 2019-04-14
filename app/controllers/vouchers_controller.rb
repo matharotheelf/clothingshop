73befe221725f92d95a2b18ed0e73585370b558c
matharotheelf <mrthomasjacoblawrence@hotmail.co.uk>
2019-04-14 23:21:13 +0100

    Refactor: extracxted voucher invalid from apply in voucher controller

--- app/controllers/vouchers_controller.rb
+++ app/controllers/vouchers_controller.rb
@@ -1,7 +1,7 @@
 class VouchersController < ApplicationController
   def apply
     @voucher = Voucher.where(name: params[:name])
-    if not_apply_ten_pound?(params[:name], params[:total_price]) || not_apply_fifteen_pound?(params[:name], params[:total_price])
+    if voucher_invalid?
       redirect_to root_url, notice: 'You can not currently use that voucher.'
     else
       @voucher.update(is_applied: true)
@@ -17,6 +17,10 @@ class VouchersController < ApplicationController
 
   def not_apply_fifteen_pound?(name, total_price)
     name == '£15.00 off when you have bought at least one footwear item and spent over £75.00' && (total_price.to_i <= 75 || not_bought_footwear)
+  end
+
+  def voucher_invalid?
+    not_apply_ten_pound?(params[:name], params[:total_price]) || not_apply_fifteen_pound?(params[:name], params[:total_price])
   end
 
   def not_bought_footwear
