50e49a14ba66abfe6d911709763229e43e481ed5
matharotheelf <mrthomasjacoblawrence@hotmail.co.uk>
2019-04-14 22:15:31 +0100

    Added warning test and space after warning

--- spec/features/vouchers_spec.rb
+++ spec/features/vouchers_spec.rb
@@ -3,6 +3,7 @@ RSpec.feature 'Vouchers', type: :feature do
     scenario 'User can see vouchers' do
       visit '/'
       expect(page).to have_content('Vouchers')
+      expect(page).to have_content('Do not apply vouchers until you know your basket is correct. Deleting an item removes all vouchers!')
       expect(page).to have_content('£5 pound off')
       expect(page).to have_content('£10.00 off when you spend over £50.00')
       expect(page).to have_content('£15.00 off when you have bought at least one footwear item and spent over £75.00')
